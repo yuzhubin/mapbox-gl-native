3a2839f767a5e4edc35618dbdbdf8d10b2b8807b
Minh Nguyễn <mxn@1ec5.org>
2017-12-21 05:30:21 -0800

    [ios, macos] Eviscerated style function tests

--- platform/darwin/test/MGLStyleValueTests.m
+++ platform/darwin/test/MGLStyleValueTests.m
@@ -11,14 +11,6 @@
 }
 
 - (void)testDeprecatedFunctions {
-    MGLShapeSource *shapeSource = [[MGLShapeSource alloc] initWithIdentifier:@"test"
-                                                                       shape:nil
-                                                                     options:nil];
-    MGLSymbolStyleLayer *symbolStyleLayer = [[MGLSymbolStyleLayer alloc] initWithIdentifier:@"symbolLayer"
-                                                                                     source:shapeSource];
-    MGLCircleStyleLayer *circleStyleLayer = [[MGLCircleStyleLayer alloc] initWithIdentifier:@"circleLayer"
-                                                                                     source:shapeSource];
-    
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
     // deprecated function, stops with float values
@@ -28,10 +20,7 @@
         @3: [MGLStyleValue<NSNumber *> valueWithRawValue:@2],
         @4: [MGLStyleValue<NSNumber *> valueWithRawValue:@0],
     };
-    MGLStyleValue<NSNumber *> *iconHaloBlurStyleValue =
-        [MGLStyleValue<NSNumber *> valueWithInterpolationBase:1.0 stops:stops];
-    symbolStyleLayer.iconHaloBlur = iconHaloBlurStyleValue;
-    XCTAssertEqualObjects(symbolStyleLayer.iconHaloBlur, iconHaloBlurStyleValue);
+    (void)[MGLStyleValue<NSNumber *> valueWithInterpolationBase:1.0 stops:stops];
     
     // deprecated function, stops with boolean values
     stops = @{
@@ -42,13 +31,12 @@
     };
     MGLStyleValue<NSNumber *> *iconAllowsOverlapStyleValue =
         [MGLStyleValue<NSNumber *> valueWithInterpolationBase:1.0 stops:stops];
-    symbolStyleLayer.iconAllowsOverlap = iconAllowsOverlapStyleValue;
     // iconAllowsOverlap is boolean so mgl and mbgl conversions will coerce the developers stops into interval stops
     MGLStyleValue<NSNumber *> *expectedIconAllowsOverlapStyleValue =
         [MGLStyleValue<NSNumber *> valueWithInterpolationMode:MGLInterpolationModeInterval
                                                   cameraStops:stops
                                                       options:nil];
-    XCTAssertEqualObjects(symbolStyleLayer.iconAllowsOverlap, expectedIconAllowsOverlapStyleValue);
+    XCTAssertEqualObjects(iconAllowsOverlapStyleValue, expectedIconAllowsOverlapStyleValue);
     
     ///
     // creating and using MGLStyleFunctions directly
@@ -61,13 +49,12 @@
     MGLStyleFunction<NSNumber *> *circleRadiusFunction =
         [MGLStyleFunction<NSNumber *> functionWithInterpolationBase:1.0
                                                               stops:circleRadiusStops];
-    circleStyleLayer.circleRadius = circleRadiusFunction;
     MGLStyleValue<NSNumber *> *expectedCircleRadiusFunction =
         [MGLStyleValue<NSNumber *> valueWithInterpolationMode:MGLInterpolationModeExponential
                                                   cameraStops:circleRadiusStops
                                                       options:nil];
     // setting a data driven property to an MGLStyleFunction should return an exponential camera function
-    XCTAssertEqualObjects(circleStyleLayer.circleRadius, expectedCircleRadiusFunction);
+    XCTAssertEqualObjects(circleRadiusFunction, expectedCircleRadiusFunction);
     
     CGVector circleTranslationOne = CGVectorMake(100, 0);
     CGVector circleTranslationTwo = CGVectorMake(0, 0);
@@ -86,13 +73,12 @@
     MGLStyleFunction<NSValue *> *circleTranslationFunction =
         [MGLStyleFunction<NSValue *> functionWithInterpolationBase:1.0
                                                              stops:circleTranslationStops];
-    circleStyleLayer.circleTranslation = circleTranslationFunction;
     MGLStyleValue<NSValue *> *expectedCircleTranslationFunction =
         [MGLStyleValue<NSValue *> valueWithInterpolationMode:MGLInterpolationModeExponential
                                                  cameraStops:circleTranslationStops
                                                      options:nil];
     // setting a non-data driven, interpolatable property to an MGLStyleFunction should return an exponential camera function
-    XCTAssertEqualObjects(circleStyleLayer.circleTranslation, expectedCircleTranslationFunction);
+    XCTAssertEqualObjects(circleTranslationFunction, expectedCircleTranslationFunction);
     
     NSDictionary<NSNumber *, MGLStyleValue<NSNumber *> *> *iconOptionalStops = @{
         @0: [MGLStyleValue<NSNumber *> valueWithRawValue:@NO],
@@ -101,12 +87,11 @@
     MGLStyleFunction<NSNumber *> *iconOptionalFunction =
         [MGLStyleFunction<NSNumber *> valueWithInterpolationBase:1.0
                                                            stops:iconOptionalStops];
-    symbolStyleLayer.iconOptional = iconOptionalFunction;
     MGLStyleValue<NSNumber *> *expectedIconOptionalFunction =
         [MGLStyleValue<NSNumber *> valueWithInterpolationMode:MGLInterpolationModeInterval
                                                   cameraStops:iconOptionalStops
                                                       options:nil];
-    XCTAssertEqualObjects(symbolStyleLayer.iconOptional, expectedIconOptionalFunction);
+    XCTAssertEqualObjects(iconOptionalFunction, expectedIconOptionalFunction);
 #pragma clang diagnostic pop
 }
 
